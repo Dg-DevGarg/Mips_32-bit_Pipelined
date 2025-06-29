0665f322979f97dd9b288dd903b6a890bfbd2f30
ayanna-m246 <ayanna_mondal@yahoo.com>
2025-06-30 00:20:22 +0530

    Update data_mem.v

--- data_mem.v
+++ data_mem.v
@@ -24,20 +24,27 @@ module data_mem(
    input [31:0] addr,
    input [31:0] write_data,
    output [31:0] read_data,
-   input MemRead, MemWrite,clk
+   input MemRead, MemWrite,clk,output [31:0] data_mem_req_output,output [29:0]check_addr
     );
     integer i;
   reg [31:0] mem [31:0];
   initial begin
 for(i=0;i<31;i=i+1) begin
+if(i!=9 && i!=10) begin
 mem[i] =32'b0;
 end
+else if(i==9) mem[9] = 32'd18;
+else if(i==10) mem[10] = 32'd118;
 end
-  reg [31:0]read_d;
+end
+  //reg [31:0]read_d;
   always@(posedge clk)begin
-  if(MemRead) read_d<=mem[addr[31:2]];
+  //if(MemRead) read_d<=mem[addr[31:2]];
   if(MemWrite) mem[addr[31:2]]<=write_data;
   end
-  
-  assign read_data=read_d;
+  assign read_data = MemRead?mem[addr[31:2]]:0;
+  //assign read_data=read_d;
+ // assign data_mem_req_output = mem[26];
+ assign data_mem_req_output = mem[10];
+  assign check_addr = addr[31:2];
 endmodule
